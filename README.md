f9f9dc4597ff6832541fa7a2e49cf2d516ee2f3b
Michael Theriot <michael.lee.theriot@gmail.com>
2015-12-10 01:18:55 -0600

    add more functions

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 This is a linked-list implementation of a queue.
 
+Multiple arguments can be taken in the constructor to add to the queue.
+
 ```javascript
 var q = new Queue();
 
@@ -14,25 +16,84 @@ q.dequeue(); // 6
 q.dequeue(); // 7
 ```
 
-Queues also have a length property and can be created from an iterable.
+## .enqueue(...item), .push(...item)
+
+Add elements to the queue using `enqueue` or its alias `push`. These can take multiple arguments and will queue the items in FIFO order.
 
 ```javascript
-var set = new Set([1,2,3]);
-var q = Queue.from(set);
+var q = new Queue();
 
-while(q.length) {
-  q.dequeue(); // 1, 2, 3
-}
+q.enqueue(5, 6, 7); // 3
+
+q.dequeue(); // 5
+q.dequeue(); // 6
+q.dequeue(); // 7
 ```
 
-Queues can also be enqueued from the constructor, and `enqueue` can take multiple arguments.
+## .dequeue(), .shift()
+
+`dequeue` and its alias `shift` removes and returns the first element in the queue.
+
+## .unshift(...item)
+
+Adds an item to the front of the queue.
+
+```javascript
+var q = new Queue(5, 6, 7);
+
+q.unshift(1); // 4
+
+q.dequeue(); // 1
+q.dequeue(); // 5
+q.dequeue(); // 6
+q.dequeue(); // 7
+```
+
+## .pop()
+
+Removes an item from the end of the queue.
+
+This method is in **O(n) time**.
+
+```javascript
+var q = new Queue(5, 6, 7);
+
+q.pop(); // 7
+```
+
+## .first()
+
+Returns the first item in the queue without removing it.
+
+## .last()
+
+Returns the last item in the queue without removing it.
+
+## .length
+
+Returns the number of items in the queue.
+
+## Iteratable
+
+Queues are an iterable and iterating them does not affect their contents.
 
 ```javascript
 var q = new Queue(1, 2, 3);
 
-q.enqueue(4, 5, 6);
+for(var item of q) {
+  console.log(item); // 1, 2, 3
+}
+```
+
+# Queue.from([[iterable]])
+
+Queues can be created from an iterable using `Queue.from`.
+
+```javascript
+var set = new Set([1, 2, 3]);
+var q = Queue.from(set);
 
 while(q.length) {
-  q.dequeue(); // 1, 2, 3, 4, 5, 6
+  q.dequeue(); // 1, 2, 3
 }
 ```
--- index.js
+++ index.js
@@ -15,9 +15,8 @@ function Queue() {
     endNode: undefined
   });
   if(arguments.length > 0) {
-    var queue = this;
     for(var i = 0; i < arguments.length; i++) {
-      queue.enqueue(arguments[i]);
+      this.enqueue(arguments[i]);
     }
   }
 }
@@ -35,7 +34,7 @@ function Queue() {
 
 Queue.prototype.enqueue = function (item) {
   var store = stores.get(this);
-  for(var i, item = arguments[i = 0]; i < arguments.length; item = arguments[++i]) {
+  for(var i = 0; i < arguments.length; item = arguments[++i]) {
     var node = new Node(item);
     if(store.endNode) {
       store.endNode = store.endNode.nextNode = node;
@@ -46,6 +45,7 @@ Queue.prototype.enqueue = function (item) {
   }
   return store.length;
 };
+
 Queue.prototype.dequeue = function () {
   var store = stores.get(this);
   var node = store.startNode;
@@ -58,10 +58,63 @@ Queue.prototype.dequeue = function () {
   }
 };
 
+Queue.prototype.unshift = function (item) {
+  var store = stores.get(this);
+  for(var i = 0; i < arguments.length; item = arguments[++i]) {
+    var node = new Node(item, store.startNode);
+    store.startNode = node;
+    if(!store.endNode) {
+      store.endNode = store.startNode;
+    }
+    store.length++;
+  }
+  return store.length;
+};
+
+Queue.prototype.pop = function () { // O(n)
+  var store = stores.get(this);
+  var node, endNode = store.endNode;
+  for(node = store.startNode; node !== store.endNode && node.nextNode !== store.endNode; node = node.nextNode);
+  if(node) {
+    (store.endNode = node).nextNode = undefined;
+    if(!--store.length) {
+      store.startNode = store.endNode = undefined;
+    }
+    return endNode.item;
+  }
+};
+
+Queue.prototype.first = function () {
+  return stores.get(this).startNode;
+};
+
+Queue.prototype.last = function () {
+  return stores.get(this).endNode;
+};
+
+Queue.prototype.push = Queue.prototype.enqueue;
+Queue.prototype.shift = Queue.prototype.dequeue;
+
 Object.defineProperty(Queue.prototype, 'length', {
   get: function () {
     return stores.get(this).length;
   }
 });
+
+Queue.prototype[Symbol.iterator] = function () {
+  var store = stores.get(this);
+  var node;
+  var finished = this.length === 0;
+  return {
+    next: function () {
+      node = node ? node.nextNode : store.startNode;
+      if(finished = finished || node === undefined) {
+        return {value: undefined, done: finished};
+      } else {
+        return {value: node.item, done: finished};
+      }
+    }
+  };
+};
 
 module.exports = Queue;
